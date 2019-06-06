e029b803201a31253f61559b23ee72951469b097
Ricardo Rodrigues <ricardo.e.p.rodrigues@gmail.com>
2019-06-06 14:06:18 +0100

    Corrected SDL2 finding for Apple.

--- cmake/FindSDL2.cmake
+++ cmake/FindSDL2.cmake
@@ -286,7 +286,8 @@ else ()
         ENDIF (MINGW)
 
         # Set the final string here so the GUI reflects the final state.
-        SET(SDL2_LIBRARIES ${SDL2_LIBRARY_TEMP} CACHE STRING "Where the SDL2 Library can be found")
+        SET(SDL2_LIBRARY ${SDL2_LIBRARY_TEMP} CACHE STRING "Where the SDL2 Library can be found")
+        set(SDL2_LIBRARIES ${SDL2_LIBRARY})
         # Set the temp variable to INTERNAL so it is not seen in the CMake GUI
         SET(SDL2_LIBRARY_TEMP "${SDL2_LIBRARY_TEMP}" CACHE INTERNAL "")
     ENDIF (SDL2_LIBRARY_TEMP)
